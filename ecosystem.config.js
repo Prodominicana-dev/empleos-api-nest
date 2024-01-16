fbc061ed0d0a0b69629d643d2ed4022430342c52
Empleos-Api <faustotineo@prodominicana.gob.do>
2024-01-16 13:59:21 -0400

    Agregando Actions

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    apps: [
+      {
+        name: 'empleos-api-nest',
+        script: 'npm',
+        args: 'start',
+        cwd: '/var/www/empleos-api-nest', // Ruta del directorio del proyecto en el servidor
+        instances: 1, // Número de instancias que deseas ejecutar
+        autorestart: true, // Hacer que la aplicación se reinicie automáticamente si falla
+        watch: false, // Desactivar el monitoreo de cambios en los archivos para reiniciar automáticamente
+        max_memory_restart: '1G', // Reiniciar la aplicación si el uso de memoria supera el límite (opcional)
+        env: {
+          NODE_ENV: 'production', // Establecer el entorno a producción (opcional)
+        },
+      },
+    ],
+  };
