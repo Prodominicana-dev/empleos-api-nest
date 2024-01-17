70ac91afdf434a2bfde73a926ba46eea9ba4faac
Empleos-Api <faustotineo@prodominicana.gob.do>
2024-01-17 09:49:57 -0400

    Cambio de listen

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Habilita el intercambio de credenciales (si es necesario)
   });
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
