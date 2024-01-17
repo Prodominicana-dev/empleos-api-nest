f40da791abe959371184356395fb528d8198d249
Empleos-Api <faustotineo@prodominicana.gob.do>
2024-01-17 09:41:27 -0400

    Ajustes para subir a producción

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001','http://localhost:3002'], // Reemplaza esto con tu dominio frontend
+    origin: ['https://empleosapi.prodominicana.gob.do','https://empleosapi.prodominicana.gob.do'], // Reemplaza esto con tu dominio frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Habilita el intercambio de credenciales (si es necesario)
   });
