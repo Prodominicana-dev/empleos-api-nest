b3648fef7a4603ab787e73e3334273577f416a4c
Fausto-Tineo <faustotineo@prodominicana.gob.do>
2024-01-18 11:40:32 -0400

    Cambio en main09

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+//import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule, { cors: {
@@ -8,7 +8,16 @@ async function bootstrap() {
   //   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
   //   credentials: true
   // } });
+
+  const app = await NestFactory.create(AppModule, { cors: true });
   // const app = await NestFactory.create(AppModule);
+
+  // // const app = await NestFactory.create(AppModule, { cors: {
+  // //   origin: ['https://empleosadmin.prodominicana.gob.do','https://empleos.prodominicana.gob.do'],
+  // //   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  // //   credentials: true
+  // // } });
+  // // const app = await NestFactory.create(AppModule);
 
   // const corsOptions: CorsOptions = {
   //   origin: '*',
@@ -17,14 +26,14 @@ async function bootstrap() {
   // };
 
   // app.enableCors(corsOptions);
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
 
-  // Configurar CORS para permitir cualquier origen
-  app.enableCors({
-    origin: '*',
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-    credentials: true,
-  });
+  // // Configurar CORS para permitir cualquier origen
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  //   credentials: true,
+  // });
 
 
 
