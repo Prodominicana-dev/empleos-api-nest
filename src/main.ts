4fb2bcea61fdd6fccee020d83a2be9226b474310
Fausto-Tineo <faustotineo@prodominicana.gob.do>
2024-01-19 12:50:35 -0400

    Cambio en main17

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,17 @@ import { AppModule } from './app.module';
 //import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, { cors: {
-  //   origin: ['https://empleosadmin.prodominicana.gob.do','https://empleos.prodominicana.gob.do'],
-  //   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-  //   credentials: true
-  // } });
-  
-   const app = await NestFactory.create(AppModule);
 
-  // Configurar CORS para permitir cualquier origen
-  app.enableCors({
-    origin: '*',
+  const app = await NestFactory.create(AppModule, { cors: {
+
+    origin: ['https://empleosadmin.prodominicana.gob.do','https://empleos.prodominicana.gob.do','http://localhost:3000','http://localhost:3001'],
     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-    credentials: true,
-  });
+    credentials: true
+
+
+  } });
+  
+  
 
 
 
