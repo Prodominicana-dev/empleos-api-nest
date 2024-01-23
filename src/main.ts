efdc61b845a8e2d044ac4ac801f162e82ee97aff
Fausto-Tineo <faustotineo@prodominicana.gob.do>
2024-01-23 11:37:47 -0400

    Cambios en MAIN08

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule, { cors: {
 
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
     origin: true,
     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
     credentials: true,
-    allowedHeaders: 'Content-Type, Authorization',
+  });
 
 
-  } });
+  // const app = await NestFactory.create(AppModule, { cors: {
+
+  //   origin: true,
+  //   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  //   credentials: true,
+  //   allowedHeaders: 'Content-Type, Authorization',
+
+
+  // } });
   
   
 
