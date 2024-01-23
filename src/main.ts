9d7f57a7b9cb7a94545aa874c99a248d31b4ffa8
Fausto-Tineo <faustotineo@prodominicana.gob.do>
2024-01-23 11:19:29 -0400

    Cambios en MAIN07

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, { cors: {
 
-    origin: ['https://empleos.prodominicana.gob.do'],
+    origin: true,
     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
